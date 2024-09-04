c8cb895521f0a97b09cafe348b0c8ff4c75e0b80
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-09-03 21:02:13 -0400

    updated js to form and blog js files

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,13 @@
 // TODO: Create a variable that selects the form element
-
+const formEl = document.querySelector("#form") 
+const submitBtn = document.querySelector("#submitBtn")
+console.log(formEl);
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+function storeData() {
+    event.preventDefault()
+    console.log("Storing data");
+    //create var for each form section
+}
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+submitBtn.addEventListener("click", storeData);
