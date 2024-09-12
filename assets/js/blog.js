42e80e90ce62dbddb83d139a7bb5da12e6557aaa
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-09-12 12:55:29 -0400

    updated HTML, CSS, and JS files

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,22 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
-
+const mainEl = document.querySelector("main")
+const backBtnEl = document.querySelector("#back")
 
 // TODO: Create a function that builds an element and appends it to the DOM
+function appendEl() {
+    const buildEl = document.body.appendChild(document.createElement("newElement"));
+}
+console.log(appendEl);
 
 // TODO: Create a function that handles the case where there are no blog posts to display
+function emptyBlog() {
+
+}
 
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+function renderBlogList() {
+    
+}
 
 // TODO: Call the `renderBlogList` function
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,49 @@
 // TODO: Create a variable that selects the form element
-const formEl = document.querySelector("#form") 
+const formEl = document.querySelector("#form")
+const username = document.querySelector("#username")
+const title = document.querySelector("#title")
+const content = document.querySelector("#content")
 const submitBtn = document.querySelector("#submitBtn")
 console.log(formEl);
+//create var for each form section
+
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+
 function storeData() {
+    // If Submit button is hit
+    // create var for localStorage 
+    const blogPost = {
+        // this var will save each sections value
+        username: username.value,
+        title: title.value,
+        content: content.value
+    }
+    // I need to make this saved data visible by using a JSON stringify
+    // this turns JS objects into strings
+    // this takes the blogPost var and stringifies it
+    const myJSON = JSON.stringify(blogPost)
+    // then I need to store the typed data to localStorage
+    localStorage.setItem("test", myJSON)
+
+    // then I need to redirect to a new page
+    window.location.href = "http://127.0.0.1:5500/blog.html"
+
+    // add an empty array, save items into empty array, push said array into local storage
+    // am I doing this right?
+    let json_array = {}
+    json_array.excludes = []
+    console.log(json_array)
+    // line 29 helps previous data not be overwritten when hitting submit
+
     event.preventDefault()
     console.log("Storing data");
-    //create var for each form section
+    console.log(username.value);
+    console.log(title.value);
+    console.log(content.value);
+    // if (storeData === "") don't know how to do this yet.. 
 }
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
 submitBtn.addEventListener("click", storeData);
+
+// then the new page needs to show each blog title listed in a div. This will be in the blog.js file
